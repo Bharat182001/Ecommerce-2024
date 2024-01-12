cab16edc0c06451012fe0dcd09e8567ca3b80f91
Bharat <databharat0007@gmail.com>
2024-01-12 14:46:20 +0530

    Home UI and Product Card

--- ecommerce-2024-frontend/src/pages/home.tsx
+++ ecommerce-2024-frontend/src/pages/home.tsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
+
 const Home = () => {
+
+  const addToCartHandler = () => {
+
+  }
+
   return (
-    <div>
-      Home
+    <div className="home">
+      <section></section>
+
+      <h1>Latest Products
+        <Link to={'/search'} className="findmore" >More</Link>
+      </h1>
+
+      {/*  Showing up product cards */}
+      <main>
+        <ProductCard 
+          productId="sampleId" 
+          name="Macbook" 
+          price={5600} 
+          stock={135}
+          handler={addToCartHandler}   
+          photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg"
+        />
+      </main>
     </div>
   )
 }
